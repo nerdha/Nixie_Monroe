e260190febf57452c4c3dc67342a1fd7f60e0dd4
Hilary Mason <h@bit.ly>
2013-05-31 20:32:09 -0400

    now with a human-readable clock!

--- nixie_monroe/nixie_monroe.ino
+++ nixie_monroe/nixie_monroe.ino
@@ -6,6 +6,9 @@ int numLines[10] = { 45, 44, 43, 42, 41, 40, 31, 30, 29, 28 };
 // Arduino pin numbers for the high end lines (selecting the tube to light up)
 int digitLines[12] = { 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6 };
 
+// constants of TIME!!
+int days_per_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 // Arduino pin number for the top button
 int buttonA = 19;
 // Arduino pin number for the bottom button
@@ -38,9 +41,10 @@ void setup() {
   TIMSK3 = 0x02; // OCR1A match interrupt
   OCR3A = 62500;
   // Timer 2 is used to refresh each tube in turn
-  TCCR2A = 0x00;
+  TCCR2A = 0x02;
   TCCR2B = 0x04;
-  TIMSK2 = 0x01;
+  TIMSK2 = 0x02;
+  OCR2A = 0x7F;
   // Reenable interrupts
   sei();
   Serial.begin(15200);
@@ -52,7 +56,14 @@ int lastDigit = -1;
 int lastNum = -1;
 
 // The default time, in seconds since the UNIX epoch
-unsigned long long epoch = 1358650000LL;
+//unsigned long long epoch = 1358650000LL;
+int hours = 12;
+int minutes = 0;
+int seconds = 0;
+int years = 13;
+int months = 05;
+int days = 31;
+
 
 // This is called every second. The array of digits should
 // be updated to reflect the current time.
@@ -62,39 +73,41 @@ void updateDigits() {
   // Update digits from epoch.
   // This is where various date modes would go (swatch time,
   // military time, etc.).
-  
-  if (1) {
-    // unix epoch; 10 digits long. Left justified.
-    digits[11] = -1;
-    digits[10] = -1;
-    unsigned long long n = epoch;
-    for (int i = 9; i >= 0; i--) {
-      digits[i] = n % 10;
-      n = n / 10;
-    }
-  }
+
+  digits[0] = hours / 10;
+  digits[1] = hours % 10;
+  digits[2] = minutes / 10;
+  digits[3] = minutes % 10;
+  digits[4] = seconds / 10;
+  digits[5] = seconds % 10;
+  digits[6] = months / 10;
+  digits[7] = months % 10;
+  digits[8] = days / 10;
+  digits[9] = days % 10;
+  digits[10] = years / 10;
+  digits[11] = years % 10;  
+
 }
 
 // This is called when "set" mode is exited. It's assumed
-// the user has set the correct time; update the epoch value
+// the user has set the correct time; update the date value
 // based on the values of the digits.
-void updateEpoch() {
-  // Update epoch from digits.
-  // This is where the various date modes would go.
-  if (1) {
-    // unix epoch editing mode
-    epoch = 0;
-    for (int i = 0; i < 10; i++) {
-      epoch *= 10;
-      epoch += digits[i];
-    }
-  }
+void updateDate() {
+  hours = digits[0]*10 + digits[1];
+  minutes = digits[2]*10 + digits[3];
+  seconds = digits[4]*10 + digits[5];
+  months = digits[6]*10 + digits[7];
+  days = digits[8]*10 + digits[9];
+  years = digits[10]*10 + digits[11];
 }
 
 // Turn off the previously lit tube and illuminate the
 // given number on the specified tube. If -1 is specified,
 // the tube is not lit.
 void setTube(int digit, int num) {
+  if (digit >= 12) {
+    digit = -1;
+  }
   if (lastDigit != -1) {
     digitalWrite(digitLines[lastDigit],LOW);
   }
@@ -165,7 +178,7 @@ void loop() {
   if ((editDigit == -1) && valueChanged) {
     // If we're not editing and the value has changed, update the epoch!
     valueChanged = false;
-    updateEpoch();
+    updateDate();
   }
   
   // Save button states
@@ -194,14 +207,15 @@ void loop() {
 const int BCYCLEN=80;
 
 // Interrupt for udating the tubes
-ISR(TIMER2_OVF_vect) {
+//ISR(TIMER2_OVF_vect) {
+ISR(TIMER2_COMPA_vect) {
   int d = digits[displayedDigit];
   if ((displayedDigit == editDigit) && (blinkCycle > (BCYCLEN/2))) {
     d = -1;
   }
   setTube(displayedDigit,d);
   displayedDigit++;
-  if (displayedDigit == 12) {
+  if (displayedDigit == 24) {
     displayedDigit = 0;
     blinkCycle = (blinkCycle+1)%BCYCLEN;
   }
@@ -210,7 +224,30 @@ ISR(TIMER2_OVF_vect) {
 // Interrupt for clock ticks
 ISR(TIMER3_COMPA_vect) {
   if (editDigit == -1) {
-    epoch++;
+    seconds++;
+    if (seconds > 59) {
+      seconds = 0;
+      minutes++;
+      if (minutes > 59) {
+        minutes = 0;
+        hours++;
+        if (hours > 23) {
+          hours = 0;
+          days++;
+          if (days > days_per_month[months]) {
+            days = 1;
+            months++;
+            if (months > 12) {
+              months = 1;
+              years++;
+              if (years > 99) {
+                years = 0;
+              }
+            }
+          }
+        }
+      }
+    }
     needsUpdate = true;
   }
 }
